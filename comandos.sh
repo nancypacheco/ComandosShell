84b1b5f697b5bf9e83c9e30c86a3fb0d4a1ecd2a
nancy <nppo1979@gmail.com>
2018-12-30 16:41:45 +0100

    scomandos para ver el sistema

---
+++ comandos.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+#Comandos en shell
+#Curso IFCD0111
+#Nancy Pacheco
+
+echo "vamos a listar los ficheros de un directorio"
+ls -la
+echo "****************************************************"
+echo "vamos a ver el usuario"
+whoami
+echo "****************************************************"
+echo "vamos a ver la fecha" 
+date
+echo "*****************************************************"
+echo "vamos a ver la version de SO"
+uname -a
+echo "*****************************************************"
+echo "vamos a ver la informaciòn de la red"
+ifconfig
+echo "*****************************************************"
+echo "vamos a ver la ocupaciòn del disco y su memoria ran"
+df -h
+echo "******************************************************"
+
+
